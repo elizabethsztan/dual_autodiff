d0a0e463d34c466d863eae5f025ae2fd755b82b7
elizabethsztan <114728359+elizabethsztan@users.noreply.github.com>
2024-11-24 11:26:25 +0000

    Added docstrings

--- dual_autodiff/tools.py
+++ dual_autodiff/tools.py
@@ -7,22 +7,22 @@ tool_store = Dict[str, tool_format]
 
 # Base implementation dictionary
 base_implementations: tool_store = {
-    # Basic Trigonometric Functions
+    # Trigonometric functions
     'sin': (np.sin, lambda x: np.cos(x)),
     'cos': (np.cos, lambda x: -np.sin(x)),
     'tan': (np.tan, lambda x: 1 / (np.cos(x)**2)),
 
-    # Hyperbolic Functions
+    # Hyperbolic functions
     'sinh': (np.sinh, lambda x: np.cosh(x)),
     'cosh': (np.cosh, lambda x: np.sinh(x)),
     'tanh': (np.tanh, lambda x: 1 / (np.cosh(x)**2)),
 
-    # Exponential and Logarithmic Functions
+    # Exponential and logarithmic functions
     'exp': (np.exp, lambda x: np.exp(x)),
     'log': (np.log, lambda x: 1 / x),
     'sqrt': (np.sqrt, lambda x: 1 / (2 * np.sqrt(x))),
 
-    # Inverse Trigonometric Functions
+    # Inverse trigonometric functions
     'arcsin': (np.arcsin, lambda x: 1 / np.sqrt(1 - x**2)),
     'arccos': (np.arccos, lambda x: -1 / np.sqrt(1 - x**2)),
     'arctan': (np.arctan, lambda x: 1 / (1 + x**2))
@@ -34,17 +34,21 @@ def add_function(name: str, func: Callable[[float], float],
     Add a new function to the implementation dictionary.
     
     Args:
-        name: Name of the function
-        func: The function implementation
-        derivative: The derivative implementation
+        name (str): name of the function
+        func (Callable[[float], float]): the function implementation
+        derivative (Callable[[float], float]): the function derivative implementation
     """
     base_implementations[name] = (func, derivative)
 
 def get_functions() -> tool_store:
-    """Get the current implementation dictionary."""
+    """
+    Get the current implementations dictionary.
+    """
     return base_implementations.copy()
 
 def remove_function(name: str) -> None:
-    """Remove a function from the implementation dictionary."""
+    """
+    Remove a function from the implementation dictionary.
+    """
     if name in base_implementations:
         del base_implementations[name]
